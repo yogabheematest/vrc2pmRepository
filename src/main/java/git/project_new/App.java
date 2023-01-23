70fc841aba8fb096b51b746008a743468457b6ff
venkatanagasai <venkatanagasai448@gmail.com>
2023-01-23 14:46:28 +0530

    project commiting by sai

---
+++ src/main/java/git/project_new/App.java
@@ -0,0 +1,13 @@
+package git.project_new;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
